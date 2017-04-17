a192dfcc4a6f2d786345f08c44428ca0dcf0e516
Benoit Tremblay <trembl.ben@gmail.com>
2017-04-16 21:58:01 -0400

    Add universal-cookie-express

---
+++ packages/universal-cookie-express/src/index.js
@@ -0,0 +1,29 @@
+import Cookies from 'universal-cookie';
+
+export default function universalCookieMiddleware() {
+  return function(req, res, next) {
+    req.universalCookies = new Cookies(req.headers.cookie, {
+      onSet(name, value, options) {
+        if (!req.cookie || res.headersSent) {
+          return;
+        }
+
+        const expressOpt = { ...options };
+        if (expressOpt.maxAge) {
+          // the standard for maxAge is seconds but express uses milliseconds
+          expressOpt.maxAge = options.maxAge * 1000;
+        }
+
+        res.cookie(name, value, options);
+      },
+      onRemove(name, options) {
+        if (!res.clearCookie || res.headersSent) {
+          return;
+        }
+
+        res.clearCookie(name, options);
+      }
+    });
+    next();
+  };
+}
