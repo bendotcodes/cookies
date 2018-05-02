0f5b65b9befa7c7cd4dfb1219b6825c718f3431d
Frank Li <frankvsense@gmail.com>
2018-05-01 17:54:20 -0700

    move index.d.ts outside and remove declare module

--- packages/react-cookie/src/index.d.ts
+++ packages/react-cookie/src/index.d.ts
@@ -1,48 +1,46 @@
-declare module 'react-cookie' {
-    import * as React from 'react';
+import * as React from 'react';
 
-    export type Cookie = string;
+export type Cookie = string;
 
-    export class Cookies {
-        get: (key: string, options?: ReactCookieGetOptions) => Cookie | undefined;
-        getAll: (options?: ReactCookieGetAllOptions) => Cookie[];
-        set(name: string, value: string, options?: ReactCookieSetOptions): void;
-        remove(name: string, options?: ReactCookieRemoveOptions): void;
-    }
-
-    export class CookiesProvider extends React.Component{ }
-    
-    type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
-
-    export function withCookies<T extends ReactCookieProps>(Component: React.ComponentType<T>): React.ComponentType<Omit<T, keyof ReactCookieProps>>;
-
-    export interface ReactCookieGetOptions {
-        doNotParse?: boolean;
-    }
-
-    export interface ReactCookieGetAllOptions {
-        doNotParse?: boolean;
-    }
-
-    export interface ReactCookieSetOptions {
-        path?: string;
-        expires?: Date;
-        maxAge?: number;
-        domain?: string;
-        secure?: boolean;
-        httpOnly?: boolean;
-    }
-
-    export interface ReactCookieRemoveOptions {
-        path?: string;
-        expires?: Date;
-        maxAge?: number;
-        domain?: string;
-        secure?: boolean;
-        httpOnly?: boolean;
-    }
-
-    export type ReactCookieProps = {
-        cookies: Cookies
-    };
+export class Cookies {
+    get: (key: string, options?: ReactCookieGetOptions) => Cookie | undefined;
+    getAll: (options?: ReactCookieGetAllOptions) => Cookie[];
+    set(name: string, value: string, options?: ReactCookieSetOptions): void;
+    remove(name: string, options?: ReactCookieRemoveOptions): void;
 }
+
+export class CookiesProvider extends React.Component{ }
+
+type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+
+export function withCookies<T extends ReactCookieProps>(Component: React.ComponentType<T>): React.ComponentType<Omit<T, keyof ReactCookieProps>>;
+
+export interface ReactCookieGetOptions {
+    doNotParse?: boolean;
+}
+
+export interface ReactCookieGetAllOptions {
+    doNotParse?: boolean;
+}
+
+export interface ReactCookieSetOptions {
+    path?: string;
+    expires?: Date;
+    maxAge?: number;
+    domain?: string;
+    secure?: boolean;
+    httpOnly?: boolean;
+}
+
+export interface ReactCookieRemoveOptions {
+    path?: string;
+    expires?: Date;
+    maxAge?: number;
+    domain?: string;
+    secure?: boolean;
+    httpOnly?: boolean;
+}
+
+export type ReactCookieProps = {
+    cookies: Cookies
+};
