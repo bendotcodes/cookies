98b8dc661942b2b6dab5c916189d34de83938283
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2018-09-12 18:29:01 +0200

    Update type

--- packages/react-cookie/src/types.ts
+++ packages/react-cookie/src/types.ts
@@ -1,9 +1,8 @@
-import Cookies from 'universal-cookie';
-import { Cookie } from 'universal-cookie';
+import Cookies, { CookiesByName } from 'universal-cookie';
 
 export type ReactCookieProps = {
     cookies?: Cookies,
-    allCookies?: { [name: string]: Cookie }
+    allCookies?: CookiesByName
     children?: any,
     ref?: React.RefObject<{}>
 };
