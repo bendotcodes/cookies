4b8d82f0cf4fbc9f7a6eaa2561414254cdc544ea
lkarmelo <lkarmelo@naumen.ru>
2019-03-23 16:06:28 +0500

    Fix useCookies not rerendering component after cookie changes value back to initial one

--- packages/react-cookie/src/useCookies.tsx
+++ packages/react-cookie/src/useCookies.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useRef, useState } from 'react';
 import { Cookie, CookieSetOptions } from 'universal-cookie';
 import CookiesContext from './CookiesContext';
 
@@ -17,14 +17,37 @@ export default function useCookies(
   const initialCookies = cookies.getAll();
   const [allCookies, setCookies] = useState(initialCookies);
 
+  //we create new callback each time allCookies changes to avoid closure remembering old allCookies value
+  const compareAndSetCbDeps = [cookies, allCookies];
+  const compareAndSet = useCallback(
+    (newCookies) => {
+      if (shouldUpdate(dependencies || null, newCookies, allCookies)) {
+        setCookies(cookies.getAll());
+      }
+    },
+    compareAndSetCbDeps
+  );
+
+  //ref is necessary, because it can be mutated, which makes it's "current" value accessible to onChange function below
+  const compareAndSetRef = useRef(compareAndSet);
+
+  //setting new "current" value to ref, so onChange function below will always get latest compareAndSet callback
+  useEffect(
+    () => {
+      compareAndSetRef.current = compareAndSet;
+    },
+    //it's important for useCallback above and this useEffect to share same dependencies,
+    //because otherwise ref may not be updated and onChange function may read old "current"
+    //value with callback's scope closed over old allCookies
+    compareAndSetCbDeps
+  );
+
   useEffect(
     () => {
       function onChange() {
         const newCookies = cookies.getAll();
 
-        if (shouldUpdate(dependencies || null, newCookies, allCookies)) {
-          setCookies(cookies.getAll());
-        }
+        compareAndSetRef.current(newCookies);
       }
 
       cookies.addChangeListener(onChange);
