6deb3308c0747927ba7fae41d3cc2b55205f823e
David Dios <david.dios@loveholidays.com>
2020-09-08 11:05:45 +0200

    convert maxAge seconds to ms in koa

--- packages/universal-cookie-koa/src/index.ts
+++ packages/universal-cookie-koa/src/index.ts
@@ -11,7 +11,13 @@ export default function universalCookieMiddleware() {
         if (change.value === undefined) {
           ctx.cookies.set(change.name, null);
         } else {
-          ctx.cookies.set(change.name, change.value, change.options);
+          const koaOpt = (<any>Object).assign({}, change.options);
+          if (koaOpt.maxAge && change.options && change.options.maxAge) {
+            // the standard for maxAge is seconds but koa uses milliseconds
+            koaOpt.maxAge = change.options.maxAge * 1000;
+          };
+
+          ctx.cookies.set(change.name, change.value, koaOpt);
         }
       }
     );
