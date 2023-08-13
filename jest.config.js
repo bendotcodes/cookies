9d643b12d1742bc9d7d8fbd0e7e463e0e742c170
Benoit Tremblay <trembl.ben@gmail.com>
2023-08-13 14:10:56 -0400

    Add github workflows

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   testEnvironment: 'jsdom',
+  collectCoverageFrom: [
+    'packages/react-cookie/src/**/*.{js,jsx,ts,tsx}',
+    'packages/universal-cookie/src/**/*.{js,jsx,ts,tsx}',
+    '!packages/react-cookie-demo/',
+    '!<rootDir>/node_modules/'
+  ]
 };
