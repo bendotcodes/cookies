6f1d878bfbc0ee58a13b08de0f44ff1860425c0e
Benoit Tremblay <trembl.ben@gmail.com>
2024-02-16 09:15:56 -0500

    Add e2e test for UMD build

--- packages/react-cookie/rollup.config.mjs
+++ packages/react-cookie/rollup.config.mjs
@@ -1,6 +1,7 @@
 import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import babel from '@rollup/plugin-babel';
 
