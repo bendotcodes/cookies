385c4c74f4977d33644458ab6bd3f310ea3291dc
Benoit Tremblay <trembl.ben@gmail.com>
2024-02-16 09:16:36 -0500

    Add e2e for UMD minified

--- e2e/universal-cookie-umd.spec.ts
+++ e2e/universal-cookie-umd.spec.ts
@@ -24,3 +24,24 @@ test('UMD build works properly', async ({ page }) => {
   );
   expect(cookieValue).toBe('helloWorld');
 });
+
+test('UMD minified build works properly', async ({ page }) => {
+  await page.goto('/');
+  await page.addScriptTag({
+    path: './packages/universal-cookie/umd/universalCookie.min.js',
+  });
+
+  const result = await page.evaluate(() => {
+    const cookies = new UniversalCookie();
+    cookies.set('testCookie', 'helloWorld');
+    const retrievedValue = cookies.get('testCookie');
+    return retrievedValue;
+  });
+
+  expect(result).toBe('helloWorld');
+
+  const cookieValue = await page.evaluate(() =>
+    new UniversalCookie().get('testCookie'),
+  );
+  expect(cookieValue).toBe('helloWorld');
+});
